f87429648c382272fc04d1df88928607e2bad5f6
Cristian Sanchez <sancas19@gmail.com>
2016-07-25 19:54:13 -0600

    Ejercicio 1
    Terminado ejercicio 1

---
+++ Cristian Sanchez - Guia 1/Ejercicio1/Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Guia 1 - Calculadora basica</title>
+    <link rel="stylesheet" href="forma.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="caja_superior">
+        <marquee><h2>Calculadora basica</h2></marquee>
+    </div>
+    <div id="caja_basica">
+    
+        <asp:TextBox ID="txtNum1" placeholder="Numero 1" CssClass="txt" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblOperacion" CssClass="resultado" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:TextBox ID="txtNum2" placeholder="Numero 2" CssClass="txt" runat="server"></asp:TextBox>
+        <br />
+        <asp:RadioButtonList ID="RadioButtonList1" CssClass="resultado" runat="server" AutoPostBack="True" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged">
+            <asp:ListItem>Sumar</asp:ListItem>
+            <asp:ListItem>Restar</asp:ListItem>
+            <asp:ListItem>Multiplicar</asp:ListItem>
+            <asp:ListItem>Dividir</asp:ListItem>
+        </asp:RadioButtonList>
+        <asp:Button ID="btnOperar" CssClass="boton" runat="server" Text="Operar" OnClick="btnOperar_Click" />
+        <br />
+        <br />
+        <asp:Label ID="lblResultado" CssClass="boton" runat="server" Text="Label"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Cristian Sanchez - Guia 1/Ejercicio1/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblOperacion.Text = "";
+        lblResultado.Text = "";
+    }
+    protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (RadioButtonList1.SelectedValue == "Sumar")
+            lblOperacion.Text = "+";
+        else if (RadioButtonList1.SelectedValue == "Restar")
+            lblOperacion.Text = "-";
+        else if (RadioButtonList1.SelectedValue == "Multiplicar")
+            lblOperacion.Text = "*";
+        else if (RadioButtonList1.SelectedValue == "Dividir")
+            lblOperacion.Text = "/";
+    }
+    protected void btnOperar_Click(object sender, EventArgs e)
+    {
+        if (RadioButtonList1.SelectedValue == "Sumar")
+            lblResultado.Text = (int.Parse(txtNum1.Text) + int.Parse(txtNum2.Text)).ToString();
+        else if (RadioButtonList1.SelectedValue == "Restar")
+            lblResultado.Text = (int.Parse(txtNum1.Text) - int.Parse(txtNum2.Text)).ToString();
+        else if (RadioButtonList1.SelectedValue == "Multiplicar")
+            lblResultado.Text = (int.Parse(txtNum1.Text) * int.Parse(txtNum2.Text)).ToString();
+        else if (RadioButtonList1.SelectedValue == "Dividir" && txtNum2.Text != "0")
+            lblResultado.Text = (int.Parse(txtNum1.Text) / int.Parse(txtNum2.Text)).ToString();
+        else
+            lblResultado.Text = "";
+    }
+}
